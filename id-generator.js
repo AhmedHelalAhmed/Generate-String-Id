b54f64d1ef38058a14f242e7d015dd073a7068d1
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2019-02-28 15:50:51 +0200

    init

---
+++ id-generator.js
@@ -0,0 +1,27 @@
+
+let generateRandomId = n => {
+
+    if (n > 0) {
+        const characters = ("abcdefghijklmnopqrstuvwxyz").split("");
+        const min = 0;
+        const max = characters.length;
+        let randomIndex = parseInt(Math.random() * (max - min) + min);
+        let stringId = '';
+
+        for (let i = 0; i < n; i++) {
+            stringId += characters[randomIndex];
+            randomIndex = parseInt(Math.random() * (max - min) + min);
+        }
+
+
+        console.log('Your Random Id of length ' + n + ' is : ', stringId);
+
+        return stringId;
+    }
+    else {
+
+        console.log("Invalid input");
+    }
+
+
+};
